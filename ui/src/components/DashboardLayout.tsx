03894df04ebb34fb43ef5cb44f1a26188907a67e
NirZamir1 <nirozamir@gmail.com>
2025-08-08 22:05:20 +0300

    added charka ui and layout

---
+++ ui/src/components/DashboardLayout.tsx
@@ -0,0 +1,40 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+const sidebarWidth = 10;
+const topBarHeight = 10;
+function DashboardLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Box
+        position={"fixed"}
+        top={0}
+        width={"100%"}
+        bg="#e6ba5dff"
+        color="white"
+        h={topBarHeight}
+      >
+        Top Bar
+      </Box>
+
+      <Box
+        position={"fixed"}
+        top={10}
+        left={0}
+        width={sidebarWidth}
+        height={`calc(100vh - ${topBarHeight}px)`}
+        bg="blue.500"
+        color="white"
+        p={4}
+        zIndex={0}
+      >
+        Sidebar
+      </Box>
+
+      <Box ml={sidebarWidth} mt={topBarHeight}>
+        {children}
+      </Box>
+    </>
+  );
+}
+
+export default DashboardLayout;
